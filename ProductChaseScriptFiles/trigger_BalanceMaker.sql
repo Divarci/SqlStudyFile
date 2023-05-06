f9a02535a818fdd71f32e2159ecf7f72d38dfcaa
Divarci <hasandivarciuk@gmail.com>
2023-05-07 00:22:20 +0100

    File system updated. Triggers Added.

---
+++ ProductChaseScriptFiles/trigger_BalanceMaker.sql
@@ -0,0 +1,51 @@
+﻿USE [ProductChase]
+GO
+
+/****** Object:  Trigger [dbo].[BALANCEMAKER]    Script Date: 07/05/2023 00:12:04 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TRIGGER [dbo].[BALANCEMAKER]
+ON [dbo].[TBLMOVEMENT]
+AFTER UPDATE
+AS
+BEGIN
+	-- Güncelleme yapılacak müşterinin ID'sini al
+    DECLARE @client_id INT
+    SELECT @client_id = CLIENT FROM INSERTED
+
+	DECLARE @client_idold INT
+    SELECT @client_idold = CLIENT FROM DELETED
+    
+    -- Yapılan satışların toplam fiyatını al
+    DECLARE @total_price DECIMAL(18,2)
+    SELECT @total_price = SUM(COST) FROM TBLMOVEMENT WHERE CLIENT = @client_id
+
+	DECLARE @total_price_old DECIMAL(18,2)
+    SELECT @total_price_old = SUM(COST) FROM TBLMOVEMENT WHERE CLIENT = @client_idold
+    
+	IF (@client_id = @client_idold)
+
+    -- Müşterinin balance sütununu güncelle
+    UPDATE TBLCLIENTS
+    SET CLIENTBALANCE = @total_price
+    WHERE CLIENTID = @client_id
+
+	IF (@client_id != @client_idold)
+	
+	UPDATE TBLCLIENTS
+    SET CLIENTBALANCE = @total_price
+    WHERE CLIENTID = @client_id
+	UPDATE TBLCLIENTS
+    SET CLIENTBALANCE = @total_price_old
+    WHERE CLIENTID = @client_idold
+END;
+GO
+
+ALTER TABLE [dbo].[TBLMOVEMENT] ENABLE TRIGGER [BALANCEMAKER]
+GO
+
+
---
+++ ProductChaseScriptFiles/trigger_BalanceReducer.sql
@@ -0,0 +1,34 @@
+﻿USE [ProductChase]
+GO
+
+/****** Object:  Trigger [dbo].[BALANCEREDUCER]    Script Date: 07/05/2023 00:13:59 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TRIGGER [dbo].[BALANCEREDUCER]
+ON [dbo].[TBLMOVEMENT]
+AFTER DELETE
+AS
+BEGIN
+	-- Güncelleme yapılacak müşterinin ID'sini al
+    DECLARE @client_id INT
+    SELECT @client_id = CLIENT FROM DELETED
+    
+    -- Yapılan satışların toplam fiyatını al
+    DECLARE @total_price DECIMAL(18,2)
+	SELECT @total_price = SUM(COST) FROM TBLMOVEMENT WHERE CLIENT = @client_id
+    
+    -- Müşterinin balance sütununu güncelle
+    UPDATE TBLCLIENTS
+    SET CLIENTBALANCE = @total_price
+    WHERE CLIENTID = @client_id
+END;
+GO
+
+ALTER TABLE [dbo].[TBLMOVEMENT] ENABLE TRIGGER [BALANCEREDUCER]
+GO
+
+
